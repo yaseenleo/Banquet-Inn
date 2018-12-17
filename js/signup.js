009d970de7e17ef5eece631ce4ac3862415c4867
Mohammad Yaseen <yaseenleo>
2018-12-17 15:48:56 +0500

    contact page ready and sign up page in progress

---
+++ js/signup.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBKqnOx3whVTOPnoc6K9AbS51nK0nUIcnY",
+    authDomain: "banquet-inn.firebaseapp.com",
+    databaseURL: "https://banquet-inn.firebaseio.com",
+    projectId: "banquet-inn",
+    storageBucket: "",
+    messagingSenderId: "84905194109"
+  };
+  firebase.initializeApp(config);
